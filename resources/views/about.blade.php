fcc56457d70a973abd0d45bcc65d0c154ddfe35a
ilhamnr1610 <ilhamramadhan16104@gmail.com>
2022-01-19 08:56:53 +0700

    second  commit

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,15 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ilham | About</title>
-</head>
-<body>
+@extends('layouts.main')
+
+@section('container')
     <h1>Halaman About</h1>
     <h3><?php echo $nama; ?></h3>
     <p><?php echo $email; ?></p>
     <img src="images/<?php echo $gambar; ?>" alt="<?php echo $gambar; ?>" width="200px">
-</body>
-</html>
+@endsection
+    
